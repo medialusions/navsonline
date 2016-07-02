69bd35e93eb36e569e437f2a43a0e1fe82981375
Zach Smith <zach@medialusions.com>
2016-07-02 12:22:40 -0600

    Init setup of models and classes.

---
+++ application/controllers/Blockout.php
@@ -0,0 +1,7 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Blockout extends CI_Controller {
+    //put your code here
+}
---
+++ application/controllers/Music.php
@@ -0,0 +1,7 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Music extends CI_Controller {
+    //put your code here
+}

1f49e6bd53679c1a07338accd0576995d5d10c00
Zach Smith <zach@medialusions.com>
2016-07-03 08:31:01 -0600

    Commenting.

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -4,31 +4,53 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class User extends CI_Controller {
 
+    /**
+     * Redirect upon knowledge of login status
+     */
     public function index() {
         $this->login();
     }
 
+    /**
+     * Loads login page and handles login actions
+     */
     public function login() {
         $data;
         $this->load->view('login_static', $data);
     }
 
+    /**
+     * Loads and gets data for schedule page
+     */
     public function schedule() {
         $data['title'] = 'Welcome';
 
         $this->load->view('landing_static', $data);
     }
 
+    /**
+     * Loads and gets data for music page
+     */
     public function music() {
         $data['title'] = 'Song Center';
 
         $this->load->view('music_static', $data);
     }
 
+    /**
+     * Loads and gets data for people page
+     */
     public function people() {
         $data['title'] = 'People';
 
         $this->load->view('people_static', $data);
+    }
+
+    /**
+     * Gets data for the left rail
+     */
+    public function get_rail() {
+        
     }
 
 }

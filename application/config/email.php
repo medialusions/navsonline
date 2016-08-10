2ab26f20babaa68c3e3aec2b382b38af8969f8f4
Zach Smith <zach@medialusions.com>
2016-08-10 16:08:36 -0600

    SMTP config.

--- application/config/email.php
+++ application/config/email.php
@@ -3,12 +3,11 @@
 /**
  * Configuration terms for the email library
  */
-$config['protocol'] = 'sendmail';
-$config['mailpath'] = 'C:/wamp64/sendmail/sendmail.exe';
-//$config['smtp_host'] = 'smtp.zoho.com';
-//$config['smtp_port'] = 465;
-//$config['smtp_crypto'] = 'ssl';
-//$config['smtp_user'] = 'info@medialusions.com';
-//$config['smtp_pass'] = 'meDiaInter14';
+$config['protocol'] = 'smtp';
+$config['smtp_host'] = 'smtp.zoho.com';
+$config['smtp_port'] = 587;
+$config['smtp_crypto'] = 'tls';
+$config['smtp_user'] = 'info@medialusions.com';
+$config['smtp_pass'] = 'meDiaInter14';
 $config['mailtype'] = 'html';
 $config['wordwrap'] = FALSE;

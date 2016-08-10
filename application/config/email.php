653baed9ab2658417ff36ee931ba7eec2921a8ea
Zach Smith <zach@medialusions.com>
2016-08-10 16:18:06 -0600

    Sendmail.

--- application/config/email.php
+++ application/config/email.php
@@ -3,11 +3,11 @@
 /**
  * Configuration terms for the email library
  */
-$config['protocol'] = 'smtp';
-$config['smtp_host'] = 'smtp.zoho.com';
-$config['smtp_port'] = 465;
-$config['smtp_crypto'] = 'ssl';
-$config['smtp_user'] = 'info@medialusions.com';
-$config['smtp_pass'] = 'meDiaInter14';
+$config['protocol'] = 'sendmail';
+//$config['smtp_host'] = 'smtp.zoho.com';
+//$config['smtp_port'] = 465;
+//$config['smtp_crypto'] = 'ssl';
+//$config['smtp_user'] = 'info@medialusions.com';
+//$config['smtp_pass'] = 'meDiaInter14';
 $config['mailtype'] = 'html';
 $config['wordwrap'] = FALSE;
